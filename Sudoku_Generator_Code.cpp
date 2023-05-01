694af7513f22bfd6b7e0cc548185064b357b3b77
JayrajLakkad <129647415+JayrajLakkad@users.noreply.github.com>
2023-05-01 20:12:28 +0530

    START : The main function

--- Sudoku_Generator_Code.cpp
+++ Sudoku_Generator_Code.cpp
@@ -512,3 +512,38 @@ void Sudoku::calculateDifficulty()
   this->difficultyLevel = B*100 + emptyCells;
 }
 // END: calculating difficulty level
+
+// START: The main function
+//! Step 13 : Create a main function
+int main(int argc, char const *argv[])
+{
+  // Initialising seed for random number generation
+  srand(time(NULL));
+
+  // Creating an instance of Sudoku
+  Sudoku *puzzle = new Sudoku();
+
+  // Creating a seed for puzzle generation
+  puzzle->createSeed();
+
+  // Generating the puzzle
+  puzzle->genPuzzle();
+
+  // Calculating difficulty of puzzle
+  puzzle->calculateDifficulty();
+
+  // testing by printing the grid
+  puzzle->printGrid();
+
+  // Printing the grid into SVG file
+  string rem = "sudokuGen";
+  string path = argv[0];
+  path = path.substr(0,path.size() - rem.size());
+  puzzle->printSVG(path);
+  cout<<"The above sudoku puzzle has been stored in puzzles.svg in current folder\n";
+  // freeing the memory
+  delete puzzle;
+
+  return 0;
+}
+// END: The main function
